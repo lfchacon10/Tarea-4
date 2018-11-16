55af5b4c858b0d96e93019b99c0b7b9e3f903b1a
lfchacon10 <lf.chacon10@uniandes.edu.co>
2018-11-15 22:56:01 -0500

    Comienzo

---
+++ ode.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include "iostream"
+using namespace std;
+
+//Derivadas respecto a X
+float derivada_x(float vx)
+{
+	return vx;
+}
+
+//Derivadas respecto a Y
+
+
+float derivada_y(float vy)
+{
+	return vy;
+}
+
