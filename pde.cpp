1c87628d5e2a285266d5b5a5f61073cea8426241
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-11-18 09:53:34 -0500

    Update pde.cpp

--- pde.cpp
+++ pde.cpp
@@ -2,10 +2,14 @@
 #include <math.h>
 #include "iostream"
 
-double k = 1.626;
-double Cp = 820;
-double p = 2.71;
+double k = 1.626;  //m2s-1
+double Cp = 820;  
+double p = 2.71;  // gcm-3
 double v= k/(Cp*p);
+
+double T = 100; // En centímetros
+double areaT = 0.50*0.50; // En metros
+
 
 int main(int argc, char const *argv[])
 {
@@ -13,3 +17,8 @@ int main(int argc, char const *argv[])
 	std::cout<<"Hola "<<v<<"\n"; 		
 	return 0;
 }
+
+
+//Caso 1
+
+double Tfrontera = 10;
