faad9c3edfd8720eb20f8d337171c4a3e8d1f9fe
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-11-18 09:40:42 -0500

    Update ode.cpp
    Avacne

--- ode.cpp
+++ ode.cpp
@@ -3,6 +3,14 @@
 #include "iostream"
 using namespace std;
 
+//Valores iniciales. 
+double g = 10; 
+double c=0.2; //Coeficiente de friccion
+double m= 0.2; // kg.
+
+float xo =0; 
+float to = 0; 
+float vo=300;
 //Derivadas respecto a X
 float derivada_x(float vx)
 {
@@ -17,3 +25,10 @@ float derivada_y(float vy)
 	return vy;
 }
 
+
+int main(int argc, char const *argv[])
+{
+	
+	std::cout<<"Hola"<<"\n"; 		
+	return 0;
+}
