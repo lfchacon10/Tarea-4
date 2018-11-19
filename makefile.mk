c45eea484e21ed693e085acbb1f46928c5f9f84f
lfchacon10 <lf.chacon10@uniandes.edu.co>
2018-11-18 21:21:21 -0500

    Funciona

--- makefile.mk
+++ makefile.mk
@@ -1,3 +1,17 @@
-all:
-	g++ ode.cpp -o ode.x
-	./ode.x
+#make file  makefile.mk
+
+
+
+
+
+datos2.dat: ./a.out
+	./a.out >> datos2.dat
+
+./a.out: pde.cpp
+	 g++ pde.cpp
+
+datos1.dat: ./a.out
+	./a.out >> datos1.dat
+
+./a.out: ode.cpp
+	 g++ ode.cpp
