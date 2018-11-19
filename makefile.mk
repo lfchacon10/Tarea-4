1e5ef72a07b98cddec6b903d156dfbf6ec90f3e1
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-11-19 13:48:18 -0500

    Update makefile.mk
    ODE COMPLETO

--- makefile.mk
+++ makefile.mk
@@ -1,17 +1,16 @@
 #make file  makefile.mk
 
+p45.png proyectiles.png: datoProyectil.txt adicionalProyectil.txt
+	python3 Plots_hw4.py
 
-
-
-
-datos2.dat: ./a.out
-	./a.out >> datos2.dat
-
-./a.out: pde.cpp
-	 g++ pde.cpp
-
-datos1.dat: ./a.out
-	./a.out >> datos1.dat
+datoProyectil.txt adicionalProyectil.txt: ./a.out
+	./a.out 
 
 ./a.out: ode.cpp
 	 g++ ode.cpp
+
+#datos2.dat: ./a.out
+#	./a.out >> datos2.dat
+
+#./a.out: pde.cpp
+#	 g++ pde.cpp
