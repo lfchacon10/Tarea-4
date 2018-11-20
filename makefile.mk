46431c956dc473525d698617480397871d7757c2
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-11-19 19:56:28 -0500

    Update makefile.mk

--- makefile.mk
+++ makefile.mk
@@ -1,5 +1,5 @@
 #make file  Resultados_hw4.pdf
-
+#FALTA ultimo txt
 
 Resultados.pdf : p45.png proyectiles.png calcitaFija0.png calcitaAbierta0.png calcitaPeriodica0.png Resultados_hw4.tex
 	pdflatex Resultados_hw4.tex
