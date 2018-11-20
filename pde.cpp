995c6880c17acac48a6e3c7957bc23057c369d4d
lfchacon10 <42391912+lfchacon10@users.noreply.github.com>
2018-11-19 20:06:23 -0500

    Update pde.cpp

--- pde.cpp
+++ pde.cpp
@@ -18,23 +18,23 @@ double areaT = 0.50*0.50; // En metros
 int main(int argc, char const *argv[])
 {
 	//Tamano del arreglo
-	int npuntos= 50;
-	float h = 50.0/(npuntos-1);
+	int p= 50;
+	float h = 50.0/(p-1);
 	int i,j; //valores "aleatorios"
-	float X[npuntos][npuntos];
-	float Y[npuntos][npuntos];
+	float X[p][p];
+	float Y[p][p];
 	// Inicializa los valores de los arreglos X y Y
-	for (i = 0; i < npuntos; ++i)
+	for (i = 0; i < p; ++i)
 	{
-		for (j = 0; j < npuntos; ++j)
+		for (j = 0; j < p; ++j)
 		{
 			X[i][j] = i*h;
 			Y[i][j] = j*h;
 		}
 	}
 	// Arreglos de la temperatura
-	float presente[npuntos][npuntos];
-	float futuro[npuntos][npuntos];
+	float presente[p][p];
+	float futuro[p][p];
 
 	int tipo = atoi(argv[1]);
 	ofstream escribir_datos(argv[2]);
@@ -50,9 +50,9 @@ int main(int argc, char const *argv[])
 	}
 
 	// Inicializa valores iniciales
-	for (i = 0; i < npuntos; ++i)
+	for (i = 0; i < p; ++i)
 	{
-		for (j = 0; j < npuntos; ++j)
+		for (j = 0; j < p; ++j)
 		{
 			if ( pow(X[i][j]-25.0, 2) + pow(Y[i][j]-25.0, 2) <= 10.0 )
 			{ // Corresponde a la varilla
@@ -69,16 +69,16 @@ int main(int argc, char const *argv[])
 	// Si hay condiciones de frontera fijas, asignar los valores
 	if (tipo==0)
 	{ 
-		for (i = 0; i < npuntos; ++i)
+		for (i = 0; i < p; ++i)
 		{
 			presente[0][i] = Tc; 
-			presente[npuntos-1][i] = Tc; 
+			presente[p-1][i] = Tc; 
 			presente[i][0] = Tc;
-			presente[i][npuntos-1] = Tc;
+			presente[i][p-1] = Tc;
 			futuro[0][i] = Tc;
-			futuro[npuntos-1][i] = Tc;
+			futuro[p-1][i] = Tc;
 			futuro[i][0] = Tc;
-			futuro[i][npuntos-1]= Tc;
+			futuro[i][p-1]= Tc;
 		}
 	}
 	int pasos = 1000;
@@ -86,9 +86,9 @@ int main(int argc, char const *argv[])
 	for ( int iT = 0; iT < pasos; ++iT)
 	{
 		float k= v*dT/pow(h,2);
-		for (i = 1; i < npuntos-1; ++i)
+		for (i = 1; i < p-1; ++i)
 		{
-			for (j = 1; j < npuntos-1; ++j)
+			for (j = 1; j < p-1; ++j)
 			{
 				if ( pow(X[i][j]-25.0, 2) + pow(Y[i][j]-25.0, 2) >= Tc )
 				{
@@ -98,33 +98,33 @@ int main(int argc, char const *argv[])
 		}
 		if (tipo==1)
 		{
-			for (i = 0; i < npuntos; ++i)
+			for (i = 0; i < p; ++i)
 			{
 				futuro[i][0] = futuro[i][1]; 
-				futuro[i][npuntos-1] = futuro[i][npuntos-2];
+				futuro[i][p-1] = futuro[i][p-2];
 				futuro[0][i] = futuro[1][i]; 
-				futuro[npuntos-1][i] = futuro[npuntos-2][i];
+				futuro[p-1][i] = futuro[p-2][i];
 			}
 		}else if (tipo==2)
 		{ 
-			for (i = 1; i < npuntos-1; ++i)
+			for (i = 1; i < p-1; ++i)
 			{
-				futuro[i][0] = presente[i][0] + k*(presente[i-1][0] + presente[i+1][0] -4.0*presente[i][0] +presente[i][npuntos-1] +presente[i][0+1]);
-				futuro[0][i] = presente[0][i] + k * (presente[npuntos-1][i] + presente[0+1][i] - 4.0*presente[0][i] + presente[0][i-1] + presente[0][i+1]);
-				futuro[i][npuntos-1] = presente[i][npuntos-1] + k*(presente[i-1][npuntos-1] + presente[i+1][npuntos-1] - 4.0*presente[i][npuntos-1] + presente[i][npuntos-2] + presente[i][0]);
+				futuro[i][0] = presente[i][0] + k*(presente[i-1][0] + presente[i+1][0] -4.0*presente[i][0] +presente[i][p-1] +presente[i][0+1]);
+				futuro[0][i] = presente[0][i] + k * (presente[p-1][i] + presente[0+1][i] - 4.0*presente[0][i] + presente[0][i-1] + presente[0][i+1]);
+				futuro[i][p-1] = presente[i][p-1] + k*(presente[i-1][p-1] + presente[i+1][p-1] - 4.0*presente[i][p-1] + presente[i][p-2] + presente[i][0]);
 
-				futuro[npuntos-1][i] = presente[npuntos-1][i] +k * (presente[npuntos-2][i] + presente[0][i] - 4.0*presente[npuntos-1][i] + presente[npuntos-1][i-1] + presente[npuntos-1][i+1]);
+				futuro[p-1][i] = presente[p-1][i] +k * (presente[p-2][i] + presente[0][i] - 4.0*presente[p-1][i] + presente[p-1][i-1] + presente[p-1][i+1]);
 			}
 
-			futuro[0][0] = presente[0][0] + k * (presente[npuntos-1][0] + presente[0+1][0] - 4.0*presente[0][0] + presente[0][npuntos-1] + presente[0][0+1]);
-			futuro[npuntos-1][0] = presente[npuntos-1][0] + k * (presente[npuntos-2][0] + presente[0][0] - 4.0*presente[npuntos-1][0] + presente[npuntos-1][npuntos-1] + presente[npuntos-1][0+1]);
-			futuro[0][npuntos-1] = presente[0][npuntos-1] + k * (presente[npuntos-1][npuntos-1] + presente[0+1][npuntos-1] - 4.0*presente[0][npuntos-1] + presente[0][npuntos-2] + presente[0][0]);
-			futuro[npuntos-1][npuntos-1] = presente[npuntos-1][npuntos-1] + k * (presente[npuntos-2][npuntos-1] + presente[0][npuntos-1] - 4.0*presente[npuntos-1][npuntos-1] + presente[npuntos-1][npuntos-2] + presente[npuntos-1][0]);
+			futuro[0][0] = presente[0][0] + k * (presente[p-1][0] + presente[0+1][0] - 4.0*presente[0][0] + presente[0][p-1] + presente[0][0+1]);
+			futuro[p-1][0] = presente[p-1][0] + k * (presente[p-2][0] + presente[0][0] - 4.0*presente[p-1][0] + presente[p-1][p-1] + presente[p-1][0+1]);
+			futuro[0][p-1] = presente[0][p-1] + k * (presente[p-1][p-1] + presente[0+1][p-1] - 4.0*presente[0][p-1] + presente[0][p-2] + presente[0][0]);
+			futuro[p-1][p-1] = presente[p-1][p-1] + k * (presente[p-2][p-1] + presente[0][p-1] - 4.0*presente[p-1][p-1] + presente[p-1][p-2] + presente[p-1][0]);
 		}
 
-		for (i = 0; i < npuntos; ++i)
+		for (i = 0; i < p; ++i)
 		{
-			for (j = 0; j < npuntos; ++j)
+			for (j = 0; j < p; ++j)
 			{
 				presente[i][j] = futuro[i][j];
 				escribir_datos << futuro[i][j] << " ";
